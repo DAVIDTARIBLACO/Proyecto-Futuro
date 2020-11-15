237c02f2b9563a191d8ca7f68d42a80e3e03e731
David Tarí <dtb.aal.vra@gmail.com>
2020-11-15 19:10:44 +0100

    crear aplicacion

---
+++ src/com/daw/Main.java
@@ -0,0 +1,23 @@
+package com.daw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String alumno = args[0];
+        Integer modulos = Integer.valueOf(args[1]);
+        Integer horasSemanales = Integer.valueOf(args[2]);
+        String resultado = veredicto(horasSemanales, modulos);
+        System.out.println("Estudiante " + alumno + " : ");
+        System.out.println(resultado);
+    }
+
+    static String veredicto(Integer horasSemanales, Integer modulos) {
+        if (horasSemanales.equals(modulos)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (horasSemanales < modulos){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+}
